af960b367bcc3c091df66ab9f582df60db895de6
Ravindra Varma <83342339+rahulravindran61@users.noreply.github.com>
2026-03-15 12:48:10 +0530

    Fix WSGI application assignment in wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-WSGI_APPLICATION = 'api.wsgi.app'
+app = get_wsgi_application()
