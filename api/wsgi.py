a4acc157a811a98e77d22680072265bc7faaa67f
Ravindra Varma <83342339+rahulravindran61@users.noreply.github.com>
2026-03-15 12:47:42 +0530

    Update WSGI application configuration

--- api/wsgi.py
+++ api/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-app = get_wsgi_application()
+WSGI_APPLICATION = 'api.wsgi.app'
